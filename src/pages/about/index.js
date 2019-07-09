ef3cf8533b7b52cb47f29c690a8aa15900c5b35f
mihaillatkin <noobopower@gmail.com>
2019-07-09 09:00:02 +0300

    about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -8,35 +8,54 @@ import * as SC from './styled'
 
 export default function AboutMe() {
   return (
-    <React.Fragment>
+    <SC.Page>
       <SC.Wrap>
         <SC.Contacts>
-          <span>Николай Чумаков</span>
-          <span>редактор</span>
-          <span>Телефон: +7 (999) 624-28-27</span>
-          <span>Telegram: <a href="https://t.me/stupidtowel">@stupidtowel</a></span>
-          <span>Почта: <a href="mailto:chumakov@tjournal.ru?subject=Работа">chumakov@tjournal.ru</a></span>
+          <span className="name">Николай Чумаков</span>
+          <span className="position">редактор</span>
+          <span className="phone">Телефон: +7 (999) 624-28-27</span>
+          <span className="telegram">Telegram: <a href="https://t.me/stupidtowel">@stupidtowel</a></span>
+          <span className="email">Почта: <a href="mailto:chumakov@tjournal.ru?subject=Работа">chumakov@tjournal.ru</a></span>
         </SC.Contacts>
         <SC.Image src={photo} alt="photo"/>
       </SC.Wrap>
-      <h2>Опыт работы</h2>
+      <h2>Опыт работы:</h2>
       <SC.Experience>
-        <h3>
-          Местро: <span>Должность</span>
-        </h3>
-        <span>
-          Информация про должность, местро, даты, что захочешь
+        <h3>TJournal</h3>
+        <span className="position">Выпускающий редактор</span>
+        <span className="additional">
+          Январь 2018 - настоящее время, Санкт-Петербург/Москва
         </span>
         <p>
-          <span>Обязанности и задачи</span>\n
+          <h4>Обязанности и задачи:</h4>
+          <span>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit,
           sed do eiusmod tempor incididunt ut labore et dolore magna
           aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
           nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
           in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
           cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </span>
         </p>
       </SC.Experience>
-    </React.Fragment>
+      <SC.Experience>
+          <h3>Disgusting Man</h3>
+          <span className="position">Выпускающий редактор</span>
+          <span className="additional">
+            Январь 2018 - настоящее время, Санкт-Петербург/Москва
+          </span>
+          <p>
+            <h4>Обязанности и задачи:</h4>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </span>
+          </p>
+        </SC.Experience>
+    </SC.Page>
   );
 }
--- src/pages/about/styled.js
+++ src/pages/about/styled.js
@@ -10,10 +10,42 @@ export const Wrap = styled.div`
   display: flex;
 `
 
-export const Contacts = styled.section`
+export const Page = styled.div`
+  margin: 20px;
+  p {
+  margin: 5px 0;
+    display: flex;
+    flex-direction: column;
+    h4 {
+      margin: 10px 0;
+    }
+  }
+`
 
+export const Contacts = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  .name {
+    font-size: xx-large;
+    font-weight: 600;
+  }
+  .position {
+    font-size: x-large;
+    color: #6b7272;
+    margin-bottom: 30px;
+  }
 `
 
 export const Experience = styled.section`
-
+  margin: 0 0 30px 0;
+  display: flex;
+  flex-direction: column;
+  h3 {
+    margin: 10px 0 0 0;
+  }
+  .additional {
+    color: #6f6f6f;
+    margin: 10px 0 0 0;
+  }
 `
