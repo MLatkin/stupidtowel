21df4e5360d0ed6841bf2e2c843b35126711a673
mihaillatkin <noobopower@gmail.com>
2019-07-09 09:10:04 +0300

    Updates

---
+++ precache-manifest.dc31d1f7cfd52b138e1fb4d874e0a79e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d180a5ff3b8293f4eeca4588121a4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dd6a1c9d3eef5067641",
+    "url": "/static/css/main.3ed78954.chunk.css"
+  },
+  {
+    "revision": "dc2894031a6bf6ef1178",
+    "url": "/static/js/2.9865fae3.chunk.js"
+  },
+  {
+    "revision": "5dd6a1c9d3eef5067641",
+    "url": "/static/js/main.e1b44ed5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aa97868cb8dd5719f14d8d5afca78a5a",
+    "url": "/static/media/Batman_0180.aa97868c.jpg"
+  },
+  {
+    "revision": "7aa7eb76a9f66f0223c8197752bb6bc5",
+    "url": "/static/media/KFOmCnqEu92Fr1Mu4WxKOzY.7aa7eb76.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/static/media/KFOmCnqEu92Fr1Mu4mxK.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "efe937997e08e15b056a3643e2734636",
+    "url": "/static/media/KFOmCnqEu92Fr1Mu5mxKOzY.efe93799.woff2"
+  },
+  {
+    "revision": "e3836d1191745d29137bfe16e4e4a2c2",
+    "url": "/static/media/KFOmCnqEu92Fr1Mu72xKOzY.e3836d11.woff2"
+  },
+  {
+    "revision": "15d8ede0a816bc7a9838207747c6620c",
+    "url": "/static/media/KFOmCnqEu92Fr1Mu7GxKOzY.15d8ede0.woff2"
+  },
+  {
+    "revision": "a835084624425dacc5e188c6973c1594",
+    "url": "/static/media/KFOmCnqEu92Fr1Mu7WxKOzY.a8350846.woff2"
+  },
+  {
+    "revision": "57993e705ff6f15e722f5f90de8836f8",
+    "url": "/static/media/KFOmCnqEu92Fr1Mu7mxKOzY.57993e70.woff2"
+  },
+  {
+    "revision": "0b93a5dc91cb53c9ccc9d3fe68a294a2",
+    "url": "/static/media/avatar.0b93a5dc.jpg"
+  },
+  {
+    "revision": "cb215d3da82c3c97345fbcd72ee1071a",
+    "url": "/static/media/mem-kot-tom-19.cb215d3d.jpg"
+  }
+]);
